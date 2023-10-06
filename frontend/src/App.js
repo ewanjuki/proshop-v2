ea313a982a00025bbb5403cbde08179deeab6297
Edwin Wanjuki <edwinwanjuki@gmail.com>
2023-10-06 07:51:01 +0300

    list products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import { Container } from "react-bootstrap";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -8,7 +10,7 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to Proshop</h1>
+          <Home />
         </Container>
       </main>
       <Footer />

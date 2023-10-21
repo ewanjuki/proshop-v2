63f27651f7399d775addc5322b4579e04920608a
Edwin Wanjuki <edwinwanjuki@gmail.com>
2023-10-21 09:44:06 +0300

    set up environment variables

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
+import dotenv from 'dotenv';
+dotenv.config();
 import products from "./data/products.js";
-const PORT = 5000;
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 

f9196ad80ea91d456b3d8b682171ff146f9a4fb8
Edwin Wanjuki <edwinwanjuki@gmail.com>
2024-02-05 08:58:18 +0300

    get products from db

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+
 dotenv.config();
+
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
+
 const PORT = process.env.PORT || 8000;
 
 connectDB(); // Connect to Mongo DB
@@ -13,13 +16,6 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((product) => product._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

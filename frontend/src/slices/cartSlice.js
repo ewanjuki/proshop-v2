74cc0f01a34a604a1042bc9d95bf04f573cd0563
Edwin Wanjuki <edwinwanjuki@gmail.com>
2024-02-08 06:20:29 +0300

    add to cart function

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -4,10 +4,46 @@ const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
   : { cartItems: [] };
 
+const addDecimals = (num) => ((num * 100) / 100).toFixed(2);
+
 const cartSlice = createSlice({
   name: "cart",
   initialState,
-  reducers: {},
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
+
+      const existingItem = state.items.find((x) => x._id === item._id);
+
+      if (existingItem) {
+        state.cartItems = state.cartItems.map((x) =>
+          x._id === existingItem._id ? item : x
+        );
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+
+      // Calculate items price
+      state.itemsPrice = addDecimals(
+        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+      );
+
+      // Calculate shipping price (If order is over $100 then free, else $10 for shipping)
+      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+
+      // Calculate tax price (15% tax)
+      state.taxPrice = addDecimals(0.15 * state.itemsPrice);
+
+      // Calculate total price
+      state.totalPrice = addDecimals(
+        state.itemsPrice + state.shippingPrice + state.taxPrice
+      );
+
+      localStorage.setItem("cart", JSON.stringify(state));
+    },
+  },
 });
+
+export const { addToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;

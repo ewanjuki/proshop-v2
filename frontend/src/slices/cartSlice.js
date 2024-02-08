5ac8b1eccf90d10abfb90599734ec73b0ca911d9
Edwin Wanjuki <edwinwanjuki@gmail.com>
2024-02-08 14:33:29 +0300

    add to cart handler

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,10 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+import { updateCart } from "../utils/cartUtils";
+
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
   : { cartItems: [] };
-
-const addDecimals = (num) => ((num * 100) / 100).toFixed(2);
 
 const cartSlice = createSlice({
   name: "cart",
@@ -13,7 +13,7 @@ const cartSlice = createSlice({
     addToCart: (state, action) => {
       const item = action.payload;
 
-      const existingItem = state.items.find((x) => x._id === item._id);
+      const existingItem = state.cartItems.find((x) => x._id === item._id);
 
       if (existingItem) {
         state.cartItems = state.cartItems.map((x) =>
@@ -23,23 +23,7 @@ const cartSlice = createSlice({
         state.cartItems = [...state.cartItems, item];
       }
 
-      // Calculate items price
-      state.itemsPrice = addDecimals(
-        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
-      );
-
-      // Calculate shipping price (If order is over $100 then free, else $10 for shipping)
-      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
-
-      // Calculate tax price (15% tax)
-      state.taxPrice = addDecimals(0.15 * state.itemsPrice);
-
-      // Calculate total price
-      state.totalPrice = addDecimals(
-        state.itemsPrice + state.shippingPrice + state.taxPrice
-      );
-
-      localStorage.setItem("cart", JSON.stringify(state));
+      updateCart(state);
     },
   },
 });
